dba7df0350176723be8a99ec59dab71c4d7af9ba
HeesooHan <hhs950824@gmail.com>
2023-07-04 17:55:22 +0900

    first commit

---
+++ docs/browser_js/js/arrays.js
@@ -0,0 +1,20 @@
+let init_array = new Array ();      // []
+
+let fruits = ["apple", "banana", "orange", "mellon"];
+
+// fruits
+// (4) ['apple', 'banana', 'orange', 'mellon']
+// fruits.length
+// 4
+// fruits.push('grape')
+// 5
+// fruits
+// (5) ['apple', 'banana', 'orange', 'mellon', 'grape']
+// fruits.indexOf('banana');
+// 1
+// fruits.join(' and ')
+// 'apple and banana and orange and mellon and grape'
+
+let fruits_second = ["berry", "strawberry"]
+
+let fruits_concat = fruits.concat(fruits_second);
---
+++ docs/browser_js/js/prints.js
@@ -0,0 +1,7 @@
+let welcome = "Hello World!";
+let numbers = 55;
+
+concats = `String: ${welcome}, Number: ${numbers}`;
+
+console.log(concats);
+
